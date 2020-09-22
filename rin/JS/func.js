c6915cc2ce37b0fb34b70138d4dde8a10716fb45
Rina <rina@gitlab.com>
2020-09-22 15:12:48 +0300

    убрала функции из другого проекта

--- rin/JS/func.js
+++ rin/JS/func.js
@@ -1,24 +1,3 @@
-function openForm(id) {
-    document.getElementById("myForm"+id).style.display = "block";
-}
-
-function closeForm(id) {
-    document.getElementById("myForm"+id).style.display = "none";
-}
-function taskStatus(id){
-    var status = $('input[name="status"'+id+']').is(":checked") ? 'done' : 'undone';
-    $.ajax( {
-        type: 'POST',
-        url: 'index.php?r=Main&action=update',
-        data: 'status='+status,
-
-        success: function(data) {
-            $('#task_form').attr('status',);
-        }
-        
-    } );
-}
-
 $('#testing_form').submit(function(){
     
     if($(this).attr('validated')=='false'){
